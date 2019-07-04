15ad698d117627917287fcd0106e3d5e471f91e9
snsn1024 <asqqqqqqqqqq@gmail.com>
2019-07-04 04:25:38 +0000

    add cart function

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 Route::get('/', 'ItemController@index');
 Route::get('/item/{item}', 'ItemController@show');
 
+Route::post('/cartitem', 'CartItemController@store');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
